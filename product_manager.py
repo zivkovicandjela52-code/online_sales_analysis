75fd83c7030bde0b85732aff3c9423ef347c5d9f
zivkovicandjela52-code <zivkovicandjela52@gmail.com>
2026-02-20 00:16:56 +0100

    dodavanje funkcije za uklanjanje proizvoda prema imenu

--- product_manager.py
+++ product_manager.py
@@ -3,3 +3,11 @@ class Product:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+def remove_product(self, product_name):
+    for product in self.products:
+        if product.name == product_name:
+            self.products.remove(product)
+            print("Proizvod uklonjen:", product_name)
+            return
+    print("Proizvod nije pronađen.")
